059dc2b15cb64cc237d563dae2736617f8ee64a2
Vladimir Quezada <a01254781@tec.mx>
2024-09-16 19:05:13 -0600

    Formatting & Transforming fetched data

--- routes/blog.js
+++ routes/blog.js
@@ -47,8 +47,18 @@ router.get('/posts/:id', async function (req, res) {
     if (!posts || posts === 0) {
         return res.status(404).render('404')
     }
+    const postData = {
+        ...posts[0],
+        date: posts[0].date.toISOString(),
+        humanReadableDate: posts[0].date.toLocaleDateString('en-US', {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        })
+    }
 
-    res.render('post-detail', { post: posts[0] })
+    res.render('post-detail', { post: postData })
 })
 
 module.exports = router
